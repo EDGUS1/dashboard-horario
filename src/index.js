4f583621d4593486d06de4b25f26c310230bef4b
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-03-01 22:05:54 -0500

    feat: Creación de contenido para renderizar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Server as WebSocketServer } from 'socket.io';
 import http from 'http';
+import Sockets from './socket';
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -8,14 +9,7 @@ const io = new WebSocketServer(httpServer);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', socket => {
-  // console.log('Nueva conexión', socket.id);
-  const address = socket.handshake;
-  console.log(address);
-  console.log(address.headers['x-forwarded-for']);
-  // console.log('New connection from ' + address.address + ':' + address.port);
-  socket.emit('connected');
-});
+Sockets(io);
 
 httpServer.listen(process.env.PORT || 3000);
 console.log('Server on port 3000');
--- src/public/main.js
+++ src/public/main.js
@@ -1,5 +1,33 @@
 const socket = io();
 
+const App = document.querySelector('#app');
+
+function crearCuadros(a) {
+  let cadena = '';
+  for (let i = 0; i < a.length; i++) {
+    cadena += `<div
+    class="cuadrado h-36 w-36 p-6 m-1 rounded ${
+      a[i].restantes > 0 ? 'bg-slate-300 hover:bg-slate-500' : 'bg-red-300'
+    } shadow cursor-pointer  flex flex-col items-center justify-center"
+  ><h1 class="text-xl font-bold italic">Dia: ${a[i].dia}</h1><p>Total: ${
+      a[i].total
+    }</p><p>Restantes: ${a[i].restantes}</p></div>`;
+  }
+  return cadena;
+}
+
 socket.on('connected', () => {
   console.log('%cServidor conectado', 'background: #222; color: #bada55');
+});
+
+socket.on('render', data => {
+  App.innerHTML = crearCuadros(data.elementos);
+  const cuadrados = document.querySelectorAll('.cuadrado');
+  cuadrados.forEach(e =>
+    e.addEventListener('click', () =>
+      socket.emit('client:select', {
+        dia: e.childNodes[0].textContent.split(' ')[1],
+      })
+    )
+  );
 });
